309116926afcf8e90d76698a24cb50e7eb892155
moricznoemi <nona.moricz@gmail.com>
2024-12-12 18:56:39 +0100

    létrehoztam a bejelentkezés és regisztáció fájlokat

---
+++ templates/kijelentkezes.php
@@ -0,0 +1,27 @@
+<?php
+if (session_status() == PHP_SESSION_NONE) {
+    session_start();
+}
+
+require_once "../config/config.php";
+
+// Kapcsolódás az adatbázishoz
+$conn = new mysqli(SERVER_NAME, USERNAME, PASSWORD, DB_NAME);
+
+// Kapcsolat ellenőrzése
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+if (isset($_SESSION['nev'])) {
+    $nev = $_SESSION['nev'];
+
+    // Bejelentkezési állapot frissítése
+    $update_sql = "UPDATE felhasznalok SET bejelentkezve = FALSE WHERE nev = '$nev'";
+    $conn->query($update_sql);
+
+    session_destroy();
+}
+
+header("Location: /beadando/");
+exit;
